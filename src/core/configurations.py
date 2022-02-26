79b83fb6c167da6037bf60d3bd85d59d59d705b6
Aldeshov <azata1919@gmail.com>
2022-02-26 21:49:18 +0600

    add license notice

--- src/core/configurations.py
+++ src/core/configurations.py
@@ -1,5 +1,22 @@
+# ADB File Explorer `tool`
+# Copyright (C) 2022  Azat Aldeshov azata1919@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import pathlib
+import platform
 
 from data.models import Device
 from helpers.tools import Singleton
@@ -8,6 +25,14 @@ from helpers.tools import Singleton
 class Application:
     __version__ = '1.0'
     __metaclass__ = Singleton
+
+    NOTICE = f"""\033[0;32m
+        ADB File Explorer v{__version__} Copyright (C) 2022  Azat Aldeshov
+        Platform {platform.platform()}
+        This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+        This is free software, and you are welcome to redistribute it
+        under certain conditions; type `show c' for details.
+    \033[0m"""
 
     PATH = pathlib.Path(__file__).parent.parent.parent.resolve()
 
